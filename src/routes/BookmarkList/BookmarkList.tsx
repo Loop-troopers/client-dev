e63adcdab7054616753fdfe9e2ab807cffbc77be
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-19 21:33:22 +0900

    feat: 북마크 페이지 추가

---
+++ src/routes/BookmarkList/BookmarkList.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding-top: var(--top-navbar-height);
+`;
+
+export default function BookmarkList() {
+  return <Wrapper>dddd</Wrapper>;
+}

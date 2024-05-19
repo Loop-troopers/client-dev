ceb4152e182dd88c4a1a6ccc36c59313d9c630b8
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-19 10:30:15 +0900

    chore: vite 프록시 서버 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://192.168.0.2:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   plugins: [react()],
 });

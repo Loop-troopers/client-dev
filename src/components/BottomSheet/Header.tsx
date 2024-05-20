6efbdb0cf075db7731fd314fb1bdc1b448e8f252
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-20 23:53:58 +0900

    feat: 바텀시트 컴포넌트 추가

---
+++ src/components/BottomSheet/Header.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles/BottomSheet.style";
+
+const Header = () => {
+  return (
+    <S.HeaderWrapper>
+      <S.HandleBar />
+    </S.HeaderWrapper>
+  );
+};
+
+export default Header;

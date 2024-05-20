2b42e502111abcfe4d639d6edec7cfc0b38ef5a8
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-20 22:35:43 +0900

    feat: 공지사항 관련 타입 추가

---
+++ src/routes/NoticeList/types/type.ts
@@ -0,0 +1,19 @@
+// 공지사항
+export interface INotice {
+  group: string;
+  noticeId: number | string;
+  title: string;
+  createdAt?: string;
+}
+
+// 클릭된 공지사항 정보 (REST api에 필요한 요소: group, notice_id)
+export type ISelectedNoticeInfo = [string, number | string];
+
+// 공지사항 상세
+export interface INoticeDetailData {
+  category: string;
+  title: string;
+  body: string;
+  imageUrls: string;
+  tables: string;
+}

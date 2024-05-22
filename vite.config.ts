ff2cbac5024b55e20fcdb21461c37d4b3a197b53
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-22 09:19:27 +0900

    chore: 프록시 서버 localhost로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://192.168.0.2:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,

407157c132b3c27d9e91d60011c363763184d54b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-22 09:14:05 +0900

    feat: useBottomSheet 훅 추가

---
+++ src/hooks/useBottomSheet.ts
@@ -0,0 +1,35 @@
+import { useAnimation } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import usePreviousValue from "./usePreviousValue";
+
+const useBottomSheet = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const controls = useAnimation();
+  const prevIsOpen = usePreviousValue(isOpen);
+
+  // @ts-expect-error framer motion elements
+  const onDragEnd = (info) => {
+    const shouldClose = info?.y > 420;
+
+    if (shouldClose) {
+      controls.start("hidden");
+      setIsOpen(false);
+    } else {
+      controls.start("visible");
+      setIsOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    if (prevIsOpen && !isOpen) {
+      controls.start("hidden");
+    } else if (!prevIsOpen && isOpen) {
+      controls.start("visible");
+    }
+  }, [controls, isOpen, prevIsOpen]);
+
+  return { onDragEnd, controls, setIsOpen, isOpen };
+};
+
+export default useBottomSheet;

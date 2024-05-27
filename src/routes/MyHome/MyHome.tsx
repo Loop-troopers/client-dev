3727eabc3cc948635686f1966e9dd36fdb4c4192
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-27 20:19:16 +0900

    feat: 로그아웃 기능 추가(임시)

--- src/routes/MyHome/MyHome.tsx
+++ src/routes/MyHome/MyHome.tsx
@@ -1,3 +1,42 @@
+import styled from "styled-components";
+import * as S from "./styles/MyHome.style";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const BgWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 10;
+`;
+
 export default function MyHome() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    // GET: 사용자 로그아웃
+    const logoutURL = `api/logout`;
+    axios
+      .get(logoutURL)
+      ?.then((res) => {
+        console.log(res);
+
+        if (res.status === 200) {
+          // userDispatch(logoutUser(res.data));
+          navigate("/");
+        }
+      })
+      ?.catch((e) => {
+        alert("로그인 실패");
+      });
+  };
+
+  return (
+    <S.Wrapper>
+      <button onClick={() => logout()}>로그아웃</button>
+    </S.Wrapper>
+  );
 }
---
+++ src/routes/MyHome/styles/MyHome.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: var(--top-navbar-height) 1rem calc(var(--btm-navbar-height) * 2);
+  background-color: #f4f6fa;
+`;

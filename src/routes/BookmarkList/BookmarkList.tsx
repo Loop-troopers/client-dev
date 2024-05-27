55a1846598a57619fe2516f11a1b0fb748524cfd
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-27 20:25:07 +0900

    feat: 북마크 페이지 추가(임시)

--- src/routes/BookmarkList/BookmarkList.tsx
+++ src/routes/BookmarkList/BookmarkList.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -6,5 +8,22 @@ const Wrapper = styled.div`
 `;
 
 export default function BookmarkList() {
-  return <Wrapper>dddd</Wrapper>;
+  useEffect(() => {
+    const saveBookmark = async () => {
+      const response = await postBookmark();
+      console.log(response);
+    };
+    saveBookmark();
+  }, []);
+
+  return <Wrapper>저장 목록</Wrapper>;
 }
+
+// POST: 북마크 저장
+const postBookmark = async () => {
+  const bookmarkURL = `/api/bookmark`;
+
+  const response = await axios.post(bookmarkURL);
+
+  return response;
+};

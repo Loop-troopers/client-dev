f9c22284cb83c2300603c7fbcfb963f943b5ae74
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-29 10:31:49 +0900

    design: 구글 폰트 적용 추가 IBM Plex Sans KR

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -14,18 +14,24 @@ export const GlobalStyle = createGlobalStyle`
     --top-navbar-height: ${TOP_NAVBAR_HEIGTH};
     --btm-navbar-height: ${BOTTOM_NAVBAR_HEIGTH};
     --btm-sheet-height: ${BOTTOM_SHEET_HEIGHT};
-    --primary-color: rgba(70, 130, 180, 1);
+    --primary-color: rgba(77, 134, 156, 0.8);
+    --secondary-color: rgba(122, 178, 178, 1);
+    --tertiary-color: rgba(205, 232, 229, 1);
+    --main-bg-color: rgba(238, 247, 255, 1);
+    
   }
 
   * {
     box-sizing: border-box;
   }
-
+  
+  
   body {
     font-family: Arial, sans-serif;
     background-color: #ffffff;
     margin: 0;
     padding: 0;
+    font-family: "IBM Plex Sans KR", sans-serif;
   }
 
   a {

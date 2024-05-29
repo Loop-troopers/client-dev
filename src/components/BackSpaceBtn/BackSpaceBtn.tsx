9ba0fbd46b6fc8caeff1cc00718535bf331267dd
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-29 15:06:57 +0900

    feat: 뒤로가기 버튼 추가

---
+++ src/components/BackSpaceBtn/BackSpaceBtn.tsx
@@ -0,0 +1,13 @@
+import { RiArrowGoBackLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const BackSpaceBtnIcon = styled(RiArrowGoBackLine)`
+  font-size: 1.8rem;
+  color: white;
+`;
+
+export default function BackSpaceBtn() {
+  const navigate = useNavigate();
+  return <BackSpaceBtnIcon onClick={() => navigate(-1)} />;
+}

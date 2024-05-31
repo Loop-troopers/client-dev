53db9c47c466b71ccfe1b3d510c89f348319d2c3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-31 16:39:35 +0900

    fix: padding-top 수정

--- src/routes/NoticeList/styles/NoticeList.style.ts
+++ src/routes/NoticeList/styles/NoticeList.style.ts
@@ -1,19 +1,9 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
   width: 100%;
-  padding: var(--top-navbar-height) 1rem calc(var(--btm-navbar-height) * 2);
+  padding: calc(var(--top-navbar-height) * 2) 1rem
+    calc(var(--btm-navbar-height) * 2);
   background-color: var(--main-bg-color);
   z-index: 0;
-`;
-
-export const FilterButtonWrapper = styled.section`
-  width: 100%;
-  height: 1rem;
-`;
-
-export const FilterButton = styled.button<{ $isSelected: boolean }>`
-  width: 3rem;
-  height: 1rem;
-  background-color: ${({ $isSelected }) => ($isSelected ? "coral" : "white")};
 `;

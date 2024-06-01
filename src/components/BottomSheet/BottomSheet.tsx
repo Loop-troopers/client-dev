1f1699230cfda9674de6d49fa7d398bc266abee9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-01 14:21:16 +0900

    feat: 바텀시트 보이는 정도 수정

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -4,26 +4,28 @@ import Header from "./Header";
 // @ts-expect-error framer motion elements
 const BottomSheet = ({ onDragEnd, controls, children }) => {
   return (
-    <S.Wrapper
-      drag="y"
-      onDragEnd={onDragEnd}
-      initial="hidden"
-      animate={controls}
-      transition={{
-        type: "spring",
-        damping: 40,
-        stiffness: 400,
-      }}
-      variants={{
-        visible: { y: 0 },
-        hidden: { y: "65%" },
-      }}
-      dragConstraints={{ top: 10 }}
-      dragElastic={0.2}
-    >
-      <Header />
-      <S.ContentWrapper>{children}</S.ContentWrapper>
-    </S.Wrapper>
+    <>
+      <S.Wrapper
+        drag="y"
+        onDragEnd={onDragEnd}
+        initial="hidden"
+        animate={controls}
+        transition={{
+          type: "spring",
+          damping: 40,
+          stiffness: 400,
+        }}
+        variants={{
+          visible: { y: -100 },
+          hidden: { y: "65%" },
+        }}
+        dragConstraints={{ top: 10 }}
+        dragElastic={0.2}
+      >
+        <Header />
+        <S.ContentWrapper>{children}</S.ContentWrapper>
+      </S.Wrapper>
+    </>
   );
 };
 

d722dc81df0db5590df0b3826c021de0ec43d5be
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-01 14:22:56 +0900

    feat: 페이지 정보 상수 추가

--- src/constants/contstants.ts
+++ src/constants/contstants.ts
@@ -5,3 +5,10 @@ export const NOTICE_GROUP = Object.freeze({
   sw_7up: "소중사",
 });
 export const NOTICE_GROUPS = Object.keys(NOTICE_GROUP);
+
+// 상단 네비게이션바 표시 페이지명
+export const PAGE_NAME = Object.freeze({
+  notices: "최신글 목록",
+  bookmark: "북마크",
+  profile: "내 정보",
+});

09988fdf74bbb39ad909159c6541c3ad93efcf57
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-16 14:30:07 +0900

    design: 북마크 목록 배경색 상수로 변경

--- src/routes/BookmarkList/styles/BookmarkList.style.ts
+++ src/routes/BookmarkList/styles/BookmarkList.style.ts
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   padding: var(--top-navbar-height) 1rem var(--btm-navbar-height);
-  background-color: white;
+  background-color: var(--main-bg-color);
 `;
 
 export const Header = styled.div`
